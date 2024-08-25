2cf0a83adde628ce850fc87f6b12b0f08b41be17
RimshaMasood <manahiljawed51@gmail.com>
2024-08-25 13:22:19 +0500

    first commit

---
+++ pro_1.dart
@@ -0,0 +1,9 @@
+void main(){
+  String UserName = "Rimsha";
+  String email ="rimsha@gmail.com";
+  String Password ="54321";
+  print("Email: $email");
+  print("Password:" + Password);
+  print(!(UserName == 'Rimshaa' || email =='rimsha@gamil.com') && Password == '54321');
+
+}
